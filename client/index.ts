4b4d9251e8a2c13f94ea3a9246d602d36c4c999b
Pedro Souza <pedrosouzadz6@pm.me>
2025-11-15 04:41:57 +1300

    feat(client): add NUI state management and weather control
    - Implement setWeather function to override weather
    - Add changeNUIState function to handle UI focus and time display

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,26 @@
-console.log("flk-base client started"); 
+let open = false;
+
+// set client Weather
+function setWeather(args: string) {
+    SetOverrideWeather(args);
+}
+
+// Change NUI State -> show | hide
+function changeNUIState(state: boolean) {
+    SetNuiFocusKeepInput(state);
+    SetNuiFocus(state, state);
+
+    const hour = GetClockHours()
+    const min = GetClockMinutes()
+
+    SendNUIMessage(
+        {
+            type: "ui",
+            display: state,
+            time: {
+                hour: String(hour).padStart(2, "0"),
+                min: String(min).padStart(2, "0"),
+            }
+        }
+    )
+}
